652c470765cd1023c98fec4d64eff41d10a86bb2
Rishav <reachrishav@gmail.com>
2023-03-05 03:02:48 +0530

    updated post api

--- functions/editBlog.js
+++ functions/editBlog.js
@@ -7,39 +7,38 @@ const client = new faunadb.Client({
 })
 
 exports.handler = async function (event) {
-  const headers = event.headers
-  console.log(process.env.POST_TOKEN)
-  if (headers["access-token"] !== process.env.POST_TOKEN) {
-    if (
-      headers["access-token"] !== undefined ||
-      process.env.POST_TOKEN !== undefined
-    ) {
-      return {
-        statusCode: 401,
-        body: JSON.stringify({ error: "Unauthorized" }),
-      }
-    }
-  }
   let blogUpdateRequest = JSON.parse(event.body)
-  const idToEdit = blogUpdateRequest.id
-  const collection = "blogs"
+  const headers = event.headers
+  if (
+    headers["access-token"] &&
+    process.env.POST_TOKEN &&
+    headers["access-token"] === process.env.POST_TOKEN
+  ) {
+    const idToEdit = blogUpdateRequest.id
+    const collection = "blogs"
 
-  await client.query(
-    q.Update(q.Ref(q.Collection(collection), idToEdit), {
-      data: {
-        title: blogUpdateRequest.title,
-        description: blogUpdateRequest.description,
-        updated_at: Date.now() * 1000,
+    await client.query(
+      q.Update(q.Ref(q.Collection(collection), idToEdit), {
+        data: {
+          title: blogUpdateRequest.title,
+          description: blogUpdateRequest.description,
+          updated_at: Date.now() * 1000,
+        },
+      })
+    )
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST",
+        "Access-Control-Allow-Headers": "Authorization, Content-Type",
+        "Content-Type": "application/json",
       },
-    })
-  )
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST",
-      "Access-Control-Allow-Headers": "Authorization, Content-Type",
-      "Content-Type": "application/json",
-    },
+    }
+  } else {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: "Unauthorized to perform this action." }),
+    }
   }
 }
--- functions/removeBlog.js
+++ functions/removeBlog.js
@@ -8,31 +8,30 @@ const client = new faunadb.Client({
 
 exports.handler = async function (event) {
   const headers = event.headers
-  if (headers["access-token"] !== process.env.POST_TOKEN) {
-    if (
-      headers["access-token"] !== undefined ||
-      process.env.POST_TOKEN !== undefined
-    ) {
-      return {
-        statusCode: 401,
-        body: JSON.stringify({ error: "Unauthorized" }),
-      }
-    }
-  }
   let blogRemovalRequest = JSON.parse(event.body)
+  if (
+    headers["access-token"] &&
+    process.env.POST_TOKEN &&
+    headers["access-token"] === process.env.POST_TOKEN
+  ) {
+    const idToDelete = blogRemovalRequest.id
+    const collection = "blogs"
 
-  const idToDelete = blogRemovalRequest.id
-  const collection = "blogs"
+    await client.query(q.Delete(q.Ref(q.Collection(collection), idToDelete)))
 
-  await client.query(q.Delete(q.Ref(q.Collection(collection), idToDelete)))
-
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST",
-      "Access-Control-Allow-Headers": "Authorization, Content-Type",
-      "Content-Type": "application/json",
-    },
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST",
+        "Access-Control-Allow-Headers": "Authorization, Content-Type",
+        "Content-Type": "application/json",
+      },
+    }
+  } else {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: "Unauthorized" }),
+    }
   }
 }
