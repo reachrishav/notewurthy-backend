b888841985e404e4ac30a3d6b04efe174bfb6ea5
Rishav <reachrishav@gmail.com>
2023-03-05 02:47:41 +0530

    added filter

--- functions/addBlog.js
+++ functions/addBlog.js
@@ -9,37 +9,36 @@ const client = new faunadb.Client({
 exports.handler = async function (event) {
   let newBlog = JSON.parse(event.body)
   const headers = event.headers
-  if (headers["access-token"] !== process.env.POST_TOKEN) {
-    if (
-      headers["access-token"] !== undefined ||
-      process.env.POST_TOKEN !== undefined
-    ) {
-      return {
-        statusCode: 401,
-        body: JSON.stringify({ error: "Unauthorized" }),
-      }
-    }
-  }
-  const createdPost = await client.query(
-    q.Create(q.Collection("blogs"), {
-      data: {
-        title: newBlog.title,
-        description: newBlog.description,
-        updated_at: Date.now() * 1000,
-        created_at: Date.now() * 1000,
-      },
-    })
-  )
+  if (
+    headers["access-token"] &&
+    process.env.POST_TOKEN &&
+    headers["access-token"] === process.env.POST_TOKEN
+  ) {
+    const createdPost = await client.query(
+      q.Create(q.Collection("blogs"), {
+        data: {
+          title: newBlog.title.toString(),
+          description: newBlog.description.toString(),
+          updated_at: Date.now() * 1000,
+          created_at: Date.now() * 1000,
+        },
+      })
+    )
 
-  console.log(newBlog)
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST",
-      "Access-Control-Allow-Headers": "Authorization, Content-Type",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(createdPost),
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST",
+        "Access-Control-Allow-Headers": "Authorization, Content-Type",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(createdPost),
+    }
+  } else {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: "Unauthorized to perform this action." }),
+    }
   }
 }
